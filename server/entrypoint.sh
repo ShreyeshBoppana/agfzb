404bdc42deb3ee8d5b1508ac7cbde5c06aa53846
ShreyeshBoppana <shreyesh.boppana@gmail.com>
2024-01-01 06:14:31 -0500

    Dock1

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,17 +1,17 @@
- #!/bin/sh
-if [ "$DATABASE" = "postgres" ]; then
-    echo "Waiting for postgres..."
+    #!/bin/sh
 
-    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-    sleep 0.1
-    done
+    if [ "$DATABASE" = "postgres" ]; then
+        echo "Waiting for postgres..."
 
-    echo "PostgreSQL started"
-fi
+        while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+        sleep 0.1
+        done
 
-# Make migrations and migrate the database.
-echo "Making migrations and migrating the database. "
+        echo "PostgreSQL started"
+    fi
 
-python manage.py makemigrations main --noinput 
-python manage.py migrate --noinput 
-exec "$@"
+    # Make migrations and migrate the database.
+    echo "Making migrations and migrating the database. "
+    python manage.py makemigrations main --noinput
+    python manage.py migrate --noinput
+    exec "$@"
